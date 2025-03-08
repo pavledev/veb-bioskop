70e75d10a578c4ab02e5db0f0a41b08d4de0adfb
Pavle <pavle_nis@yahoo.com>
2025-03-08 07:46:35 +0100

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'filmovi', component: MoviesComponent },
+    { path: 'prijava', component: LoginComponent },
+];
