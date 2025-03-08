6110ebc3e34f1feb1cde1b9d491312e65679deca
Pavle <pavle_nis@yahoo.com>
2025-03-08 07:48:47 +0100

    Add Firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+        provideAuth(() => getAuth())
+    ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDP8He-Us469h2yLm3hgoALClZpTQttGO8",
+        authDomain: "veb-bioskop.firebaseapp.com",
+        projectId: "veb-bioskop",
+        storageBucket: "veb-bioskop.firebasestorage.app",
+        messagingSenderId: "332833363323",
+        appId: "1:332833363323:web:a1fcadbe584179020ad9de"
+    }
+}
