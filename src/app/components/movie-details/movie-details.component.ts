58fde880227af478fab8986a8709e918423fc1cd
Pavle <pavle_nis@yahoo.com>
2025-03-09 11:17:34 +0100

    Update movie-details.component.ts

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { AxiosError } from 'axios';
+import { MovieModel } from '../../models/movie.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-movie-details',
-  imports: [],
-  templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css'
+    selector: 'app-movie-details',
+    imports: [],
+    templateUrl: './movie-details.component.html',
+    styleUrl: './movie-details.component.css',
+    providers: [MovieService]
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit
+{
+    private route = inject(ActivatedRoute);
+    public movie: MovieModel | null = null;
+    public error: string | null = null;
 
+    constructor(private movieService: MovieService)
+    {
+    }
+
+    ngOnInit(): void
+    {
+        const movieSlug: string | null = this.route.snapshot.paramMap.get('slug');
+
+        this.movieService
+            .getMovieDetails(movieSlug)
+            .then(response =>
+            {
+                this.movie = {
+                    movieId: response.data.id,
+                    imdbId: 0,
+                    title: response.data.title,
+                    originalTitle: response.data.titleOriginalCalculated,
+                    description: response.data.synopsis,
+                    startDate: response.data.startDate,
+                    duration: response.data.runTime,
+                    posterPath: response.data.posterImage,
+                    trailerUrl: response.data.trailerUrl,
+                    genres: response.data.genres,
+                    gallery: response.data.gallery,
+                    actors: response.data.actors,
+                    directors: response.data.directors,
+                    technologies: ["2D"],
+                };
+            })
+            .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`);
+    }
 }
