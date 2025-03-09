f109a55d4e76d9514e0f6101a7e6a7cbfcad93e0
Pavle <pavle_nis@yahoo.com>
2025-03-09 08:12:53 +0100

    Add route for registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'filmovi', component: MoviesComponent },
     { path: 'prijava', component: LoginComponent },
+    { path: 'registracija', component: RegisterComponent },
 ];
