d3059732317cc5c19d60703a93a66c1ec17e4d49
Pavle <pavle_nis@yahoo.com>
2025-03-09 08:13:26 +0100

    Update user.model.ts

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,14 @@
+import { AddressModel } from './address.model';
+
 export interface UserModel
 {
     userId: number;
+    username: string;
     passwordHash: string;
     email: string;
     firstName: string;
     lastName: string;
     phoneNumber: string;
+    address: AddressModel;
     favoriteGenres: string[];
 }
