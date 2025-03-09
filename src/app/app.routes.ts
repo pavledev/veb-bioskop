34f15b53bd3c5d39147ccdb4420b031a70aa3c10
Pavle <pavle_nis@yahoo.com>
2025-03-09 11:17:15 +0100

    Add route for movie details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { RegisterComponent } from './components/register/register.component';
+import {MovieDetailsComponent} from './components/movie-details/movie-details.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'filmovi', component: MoviesComponent },
+    { path: 'filmovi/:slug', component: MovieDetailsComponent },
     { path: 'prijava', component: LoginComponent },
     { path: 'registracija', component: RegisterComponent },
 ];
