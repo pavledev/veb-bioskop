081701a7f54d77e85170b6e607a240592186082b
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:19:05 +0100

    Move login and register components to auth folder

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,75 @@
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { AuthService } from '../../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+    selector: 'app-login',
+    imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        RouterLink,
+        FormsModule,
+        ReactiveFormsModule,
+        MatProgressSpinner
+    ],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css'
+})
+export class LoginComponent
+{
+    private router: Router = inject(Router);
+    private formBuilder: FormBuilder = inject(FormBuilder);
+    private snackBar: MatSnackBar = inject(MatSnackBar);
+    private authService: AuthService = inject(AuthService);
+    public loginForm: FormGroup;
+
+    public isLoggingIn: boolean = false;
+
+    constructor()
+    {
+        this.loginForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+        });
+    }
+
+    async loginUser()
+    {
+        if (this.loginForm.invalid)
+        {
+            return;
+        }
+
+        this.isLoggingIn = true;
+        this.loginForm.disable();
+
+        const [error, firebaseUser] = await this.authService.login(this.loginForm.value.email, this.loginForm.value.password);
+
+        this.isLoggingIn = false;
+        this.loginForm.enable();
+
+        if (error)
+        {
+            this.snackBar.open(error as string, 'Zatvori', {
+                duration: 3000,
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+            });
+        }
+        else
+        {
+            this.router.navigateByUrl('/');
+        }
+    }
+}
---
+++ src/app/components/auth/register/register.component.ts
@@ -0,0 +1,141 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule, MatPrefix } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    ValidationErrors,
+    Validators
+} from '@angular/forms';
+import { GenreService } from '../../../services/genre.service';
+import { MatSelectModule } from '@angular/material/select';
+import { LoadingComponent } from '../../loading/loading.component';
+import { ErrorComponent } from '../../error/error.component';
+import { AuthService } from '../../../services/auth.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgxMaskDirective } from 'ngx-mask';
+
+@Component({
+    selector: 'app-register',
+    imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        RouterLink,
+        MatPrefix,
+        ReactiveFormsModule,
+        MatSelectModule,
+        LoadingComponent,
+        FormsModule,
+        ErrorComponent,
+        MatProgressSpinnerModule,
+        NgxMaskDirective
+    ],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css',
+    providers: [GenreService]
+})
+export class RegisterComponent implements OnInit
+{
+    private router: Router = inject(Router);
+    private formBuilder: FormBuilder = inject(FormBuilder);
+    private snackBar: MatSnackBar = inject(MatSnackBar);
+    private genreService: GenreService = inject(GenreService);
+    private authService: AuthService = inject(AuthService);
+    public registerForm: FormGroup;
+
+    public genres: string[] | null = null;
+    public error: string | null = null
+    public isRegistering: boolean = false;
+
+    constructor()
+    {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', [Validators.required, Validators.minLength(2)]],
+            lastName: ['', [Validators.required, Validators.minLength(2)]],
+            email: ['', [Validators.required, Validators.email]],
+            username: ['', [Validators.required, Validators.minLength(6)]],
+            password: ['', [Validators.required, Validators.minLength(8)]],
+            confirmPassword: ['', [Validators.required, this.validateSamePassword]],
+            phoneNumber: ['', [Validators.required]],
+            city: ['', [Validators.required]],
+            streetName: ['', [Validators.required]],
+            streetNumber: ['', [Validators.required]],
+            favoriteGenres: ['', [Validators.required]]
+        });
+
+        this.registerForm.get('password')?.valueChanges.subscribe(() =>
+        {
+            this.registerForm.get('confirmPassword')?.updateValueAndValidity();
+        });
+    }
+
+    async ngOnInit()
+    {
+        const [error, genres] = await this.genreService.getGenres();
+
+        if (error)
+        {
+            this.error = error.message;
+        }
+        else
+        {
+            this.genres = genres;
+        }
+    }
+
+    private validateSamePassword(control: AbstractControl): ValidationErrors | null
+    {
+        const password = control.parent?.get('password');
+        const confirmPassword = control.parent?.get('confirmPassword');
+
+        return password?.value == confirmPassword?.value ? null : { 'notSame': true };
+    }
+
+    async registerUser()
+    {
+        if (this.registerForm.invalid)
+        {
+            return;
+        }
+
+        const { confirmPassword, ...user } = this.registerForm.value;
+
+        this.isRegistering = true;
+        this.registerForm.disable();
+
+        const [error, firebaseUser] = await this.authService.register(user);
+
+        this.isRegistering = false;
+        this.registerForm.enable();
+
+        if (error)
+        {
+            this.snackBar.open(error as string, 'Zatvori', {
+                duration: 3000,
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+            });
+        }
+        else
+        {
+            this.snackBar.open('Usprešna registracija.', 'Zatvori', {
+                duration: 3000,
+                horizontalPosition: 'center',
+                verticalPosition: 'top'
+            });
+
+            this.router.navigateByUrl('prijava');
+        }
+    }
+}
