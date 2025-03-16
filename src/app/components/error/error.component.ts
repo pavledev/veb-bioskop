1ac071f14d1c5ac2649c9b37f3a12ad608176616
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:24:20 +0100

    Add ErrorComponent

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-error',
+    imports: [],
+    templateUrl: './error.component.html',
+    styleUrl: './error.component.css'
+})
+export class ErrorComponent
+{
+    @Input() errorMessage: string = '';
+}
