d45e5270d629b4a5828ae5dbfcfb299ad4251b1d
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:27:15 +0100

    Update movie details component

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,25 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { MovieService } from '../../services/movie.service';
 import { AxiosError } from 'axios';
 import { MovieModel } from '../../models/movie.model';
 import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { LoadingComponent } from '../loading/loading.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { PosterDialogComponent } from '../poster-dialog/poster-dialog.component';
+import { GalleryDialogComponent } from '../gallery-dialog/gallery-dialog.component';
 
 @Component({
     selector: 'app-movie-details',
-    imports: [],
+    imports: [
+        LoadingComponent,
+        DatePipe,
+        MatButtonModule,
+        MatIcon
+    ],
     templateUrl: './movie-details.component.html',
     styleUrl: './movie-details.component.css',
     providers: [MovieService]
 })
 export class MovieDetailsComponent implements OnInit
 {
-    private route = inject(ActivatedRoute);
+    private movieService: MovieService = inject(MovieService);
+    private route: ActivatedRoute = inject(ActivatedRoute);
+    private readonly dialog: MatDialog = inject(MatDialog);
     public movie: MovieModel | null = null;
     public error: string | null = null;
 
-    constructor(private movieService: MovieService)
-    {
-    }
+    @ViewChild('galleryContainer', { static: false }) galleryContainer!: ElementRef;
 
     ngOnInit(): void
     {
@@ -31,7 +43,6 @@ export class MovieDetailsComponent implements OnInit
             {
                 this.movie = {
                     movieId: response.data.id,
-                    imdbId: 0,
                     title: response.data.title,
                     originalTitle: response.data.titleOriginalCalculated,
                     description: response.data.synopsis,
@@ -43,9 +54,69 @@ export class MovieDetailsComponent implements OnInit
                     gallery: response.data.gallery,
                     actors: response.data.actors,
                     directors: response.data.directors,
-                    technologies: ["2D"],
+                    technologies: [response.data.availableTechCMS[0].Description.substring(3, 5)],
+                    distributorName: response.data.distributorName,
                 };
             })
             .catch((e: AxiosError) => this.error = `${e.code}: ${e.message}`);
+    }
+
+    prevImage(): void
+    {
+        if (this.galleryContainer)
+        {
+            this.galleryContainer.nativeElement.scrollBy({ left: -320, behavior: 'smooth' });
+        }
+    }
+
+    nextImage(): void
+    {
+        if (this.galleryContainer)
+        {
+            this.galleryContainer.nativeElement.scrollBy({ left: 320, behavior: 'smooth' });
+        }
+    }
+
+    formatDuration(duration: number): string
+    {
+        const hours = Math.floor(duration / 60);
+        const minutes = duration % 60;
+
+        return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`;
+    }
+
+    openPosterModal(): void
+    {
+        if (!this.movie)
+        {
+            return;
+        }
+
+        this.dialog.open(PosterDialogComponent, {
+            data: {
+                posterPath: this.movie.posterPath,
+                title: this.movie.title
+            },
+            width: '50vw',
+            panelClass: 'custom-container'
+        });
+    }
+
+    openGalleryDialog(index: number): void
+    {
+        if (!this.movie)
+        {
+            return;
+        }
+
+        this.dialog.open(GalleryDialogComponent, {
+            data: {
+                images: this.movie.gallery,
+                index: index
+            },
+            width: '60vw',
+            maxWidth: '80vh',
+            panelClass: 'custom-container'
+        });
     }
 }
