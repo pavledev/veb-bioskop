528dc4a9457f313e83fd7f92ee9bea65f827c000
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:24:32 +0100

    Add PosterDialogComponent

---
+++ src/app/components/poster-dialog/poster-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+    selector: 'app-poster-dialog',
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+    ],
+    templateUrl: './poster-dialog.component.html',
+    styleUrl: './poster-dialog.component.css'
+})
+export class PosterDialogComponent
+{
+    readonly dialogRef = inject(MatDialogRef<PosterDialogComponent>);
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { posterPath: string; title: string })
+    {
+    }
+
+    close(): void
+    {
+        this.dialogRef.close();
+    }
+}
