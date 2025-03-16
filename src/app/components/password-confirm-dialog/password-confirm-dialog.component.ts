83a6c0fdb381a9d2d241aabf8eb308e232f45f06
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:24:46 +0100

    Add PasswordConfirmDialogComponent

---
+++ src/app/components/password-confirm-dialog/password-confirm-dialog.component.ts
@@ -0,0 +1,72 @@
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../services/auth.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+
+@Component({
+    selector: 'app-password-confirm-dialog',
+    imports: [
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatButtonModule,
+        MatProgressSpinner
+    ],
+    templateUrl: './password-confirm-dialog.component.html',
+    styleUrl: './password-confirm-dialog.component.css'
+})
+export class PasswordConfirmDialogComponent
+{
+    private readonly dialogRef: MatDialogRef<PasswordConfirmDialogComponent> = inject(MatDialogRef<PasswordConfirmDialogComponent>);
+    private formBuilder: FormBuilder = inject(FormBuilder);
+    private authService: AuthService = inject(AuthService);
+    public passwordForm: FormGroup;
+
+    public isReauthenticating: boolean = false;
+
+    constructor()
+    {
+        this.passwordForm = this.formBuilder.group({
+            password: ['', [Validators.required]]
+        });
+    }
+
+    async confirm()
+    {
+        if (this.passwordForm.invalid)
+        {
+            return;
+        }
+
+        const currentPassword: string = this.passwordForm.value.password;
+
+        this.isReauthenticating = true;
+        this.passwordForm.disable();
+
+        const reauthError = await this.authService.reauthenticateUser(currentPassword);
+
+        this.isReauthenticating = false;
+        this.passwordForm.enable();
+
+        if (reauthError)
+        {
+            this.passwordForm.get('password')?.setErrors({ authError: reauthError });
+        }
+        else
+        {
+            this.dialogRef.close(currentPassword);
+        }
+    }
+
+    cancel()
+    {
+        this.dialogRef.close(null);
+    }
+}
