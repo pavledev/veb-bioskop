4cca3497b42eb6cfe9e1d685e8e9d3f4a386b3cd
Pavle <pavle_nis@yahoo.com>
2025-03-16 08:28:47 +0100

    Add auth implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { User } from '@angular/fire/auth';
+import { AuthService } from './services/auth.service';
+import { AsyncPipe } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, MatToolbar, MatButtonModule, RouterLink, MatIcon],
+    imports: [RouterOutlet, MatToolbar, MatButtonModule, RouterLink, MatIcon, AsyncPipe, MatMenuModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent
 {
-    title = 'veb-bioskop';
+    private router: Router = inject(Router);
+    private authService: AuthService = inject(AuthService);
+
+    public user$: Observable<User | null> = this.authService.user$;
+
+    async logout()
+    {
+        await this.authService.logout();
+
+        this.router.navigateByUrl('/');
+    }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,241 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+    Auth,
+    AuthErrorCodes,
+    authState,
+    createUserWithEmailAndPassword, reauthenticateWithCredential,
+    signInWithEmailAndPassword,
+    signOut,
+    updateEmail,
+    updatePassword,
+    updateProfile,
+    User, verifyBeforeUpdateEmail,
+    EmailAuthProvider
+} from '@angular/fire/auth';
+import { UserModel } from '../models/user.model';
+import { doc, Firestore, FirestoreError, getDoc, setDoc, updateDoc } from '@angular/fire/firestore';
+import { UtilityService } from './utility.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService
 {
+    private firebaseAuth: Auth = inject(Auth);
+    private firestore: Firestore = inject(Firestore);
+    private utilityService: UtilityService = inject(UtilityService);
+    private userSubject = new BehaviorSubject<User | null>(null);
+    private userDocumentSubject = new BehaviorSubject<UserModel | null>(null);
+
+    constructor()
+    {
+        authState(this.firebaseAuth).subscribe(async (user) =>
+        {
+            this.userSubject.next(user);
+
+            if (user)
+            {
+                await this.getUserDocument(user.uid);
+            }
+            else
+            {
+                this.userDocumentSubject.next(null);
+            }
+        });
+    }
+
+    get currentUser(): User | null
+    {
+        return this.firebaseAuth.currentUser;
+    }
+
+    get user$(): Observable<User | null>
+    {
+        return this.userSubject.asObservable();
+    }
+
+    get userDocument$(): Observable<UserModel | null>
+    {
+        return this.userDocumentSubject.asObservable();
+    }
+
+    async register(user: UserModel)
+    {
+        let errorMessage = 'Došlo je do greške prilikom registracije!';
+        const [authError, firebaseUser] = await this.utilityService.catchError(
+            createUserWithEmailAndPassword(this.firebaseAuth,
+                user.email,
+                user.password)
+        );
+
+        if (authError)
+        {
+            const error = authError as FirestoreError;
+            const code = error.code as string;
+
+            if (code === AuthErrorCodes.EMAIL_EXISTS)
+            {
+                errorMessage = 'Već postoji korisnik sa ovom email adresom!';
+            }
+
+            return [errorMessage];
+        }
+
+        const [profileError] = await this.utilityService.catchError(
+            updateProfile(firebaseUser.user, { displayName: user.username })
+        );
+
+        if (profileError)
+        {
+            return [errorMessage];
+        }
+
+        const [dbError] = await this.utilityService.catchError(
+            setDoc(doc(this.firestore, "users", firebaseUser.user.uid), user)
+        );
+
+        if (dbError)
+        {
+            return [errorMessage];
+        }
+
+        await signOut(this.firebaseAuth);
+
+        return ['', firebaseUser];
+    }
+
+    async login(email: string, password: string)
+    {
+        const [authError, firebaseUser] = await this.utilityService.catchError(
+            signInWithEmailAndPassword(this.firebaseAuth, email, password)
+        );
+
+        if (authError)
+        {
+            const error = authError as FirestoreError;
+            const code = error.code as string;
+
+            if (code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS)
+            {
+                return ['Korisnik nije pronadjen ili je neispravna lozinka!'];
+            }
+
+            return ['Došlo je do greške prilikom prijavljivanja!'];
+        }
+
+        return ['', firebaseUser];
+    }
+
+    async logout()
+    {
+        await signOut(this.firebaseAuth);
+
+        this.userSubject.next(null);
+        this.userDocumentSubject.next(null);
+    }
+
+    async getUserDocument(uid: string)
+    {
+        const userDocReference = doc(this.firestore, 'users', uid);
+        const userDocSnapshot = await getDoc(userDocReference);
+
+        if (userDocSnapshot.exists())
+        {
+            this.userDocumentSubject.next(userDocSnapshot.data() as UserModel);
+        }
+        else
+        {
+            this.userDocumentSubject.next(null);
+        }
+    }
+
+    async updateUserProfile(uid: string, user: Partial<UserModel>)
+    {
+        const error = await this.updateFirebaseUser(user);
+
+        if (error)
+        {
+            return error;
+        }
+
+        return await this.updateUserDocument(uid, user);
+    }
+
+    async updateFirebaseUser(user: Partial<UserModel>)
+    {
+        const currentUser = this.firebaseAuth.currentUser;
+
+        if (!currentUser)
+        {
+            return 'Morate biti prijavljeni da biste ažurirali profil.';
+        }
+
+        let errorMessage = 'Došlo je do greške prilikom ažuriranja profila!';
+
+        if (user.email)
+        {
+            const [emailError] = await this.utilityService.catchError(updateEmail(currentUser, user.email));
+
+            if (emailError)
+            {
+                const error = emailError as FirestoreError;
+                const code = error.code as string;
+
+                if (code === AuthErrorCodes.EMAIL_EXISTS)
+                {
+                    errorMessage = 'Već postoji korisnik sa ovom email adresom!';
+                }
+
+                return errorMessage;
+            }
+        }
+
+        if (user.password)
+        {
+            const [passwordError] = await this.utilityService.catchError(updatePassword(currentUser, user.password));
+
+            if (passwordError)
+            {
+                return errorMessage;
+            }
+        }
+
+        if (user.username)
+        {
+            const [profileError] = await this.utilityService.catchError(updateProfile(currentUser, { displayName: user.username }));
+
+            if (profileError)
+            {
+                return errorMessage;
+            }
+        }
+
+        return null;
+    }
+
+    async updateUserDocument(uid: string, user: Partial<UserModel>)
+    {
+        const userDocReference = doc(this.firestore, "users", uid);
+        const [error] = await this.utilityService.catchError(
+            updateDoc(userDocReference, user)
+        );
+
+        return error ? 'Došlo je do greške prilikom ažuriranja profila!' : null;
+    }
+
+    async reauthenticateUser(password: string)
+    {
+        const currentUser = this.firebaseAuth.currentUser;
+
+        if (!currentUser)
+        {
+            return "Morate biti prijavljeni da biste ažurirali profil.";
+        }
+
+        const credential = EmailAuthProvider.credential(currentUser.email!, password);
+        const [reauthError] = await this.utilityService.catchError(reauthenticateWithCredential(currentUser, credential));
+
+        return reauthError ? "Pogrešna lozinka. Pokušajte ponovo." : null;
+    }
 }
