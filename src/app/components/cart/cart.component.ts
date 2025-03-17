aa8d9f788ee1587b246f65f56b274b86cc951228
Pavle <pavle_nis@yahoo.com>
2025-03-17 19:33:35 +0100

    Update cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-cart',
-  imports: [],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+    selector: 'app-cart',
+    imports: [MatCardModule, MatIconModule, MatButtonModule],
+    templateUrl: './cart.component.html',
+    styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent
+{
 
 }
