909b5e2891859dc86be408416393ac3b48726d6d
Pavle <pavle_nis@yahoo.com>
2025-03-17 17:07:26 +0100

    Update genre.service.ts

--- src/app/services/genre.service.ts
+++ src/app/services/genre.service.ts
@@ -1,23 +1,19 @@
-import { inject, Injectable, Injector, runInInjectionContext } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 import { UtilityService } from './utility.service';
 
 @Injectable()
 export class GenreService
 {
-    private injector = inject(Injector);
     private firestore = inject(Firestore);
     private utilityService = inject(UtilityService);
 
     async getGenres()
     {
-        return runInInjectionContext(this.injector, async () =>
-        {
-            const genresCollection = collection(this.firestore, 'genres');
-            const promise = getDocs(genresCollection)
-                .then(querySnapshot => querySnapshot.docs.map(doc => doc.data()['name']));
+        const genresCollection = collection(this.firestore, 'genres');
+        const promise = getDocs(genresCollection)
+            .then(querySnapshot => querySnapshot.docs.map(doc => doc.data()['name']));
 
-            return this.utilityService.catchError(promise);
-        });
+        return this.utilityService.catchError(promise);
     }
 }
