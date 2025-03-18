522dab1a3eb6b11f2a5d71efa2271b3f53a63240
Pavle <pavle_nis@yahoo.com>
2025-03-18 11:42:08 +0100

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,29 +2,35 @@ import { inject, Injectable } from '@angular/core';
 import { doc, Firestore, getDoc } from '@angular/fire/firestore';
 import { UserModel } from '../models/user.model';
 import { UtilityService } from './utility.service';
+import { from, map, Observable } from 'rxjs';
 
 @Injectable()
 export class UserService
 {
     private firestore: Firestore = inject(Firestore);
     private utilityService: UtilityService = inject(UtilityService);
-    async getUsername(userId: string)
+
+    getUsername(userId: string): Observable<string | null>
     {
         const userDocReference = doc(this.firestore, `users/${userId}`);
-        const [error, snapshot] = await this.utilityService.catchError(getDoc(userDocReference));
 
-        if (error)
-        {
-            return null;
-        }
+        return from(this.utilityService.catchError(getDoc(userDocReference))).pipe(
+            map(([error, snapshot]) =>
+            {
+                if (error)
+                {
+                    return null;
+                }
 
-        if (snapshot.exists())
-        {
-            const user = snapshot.data() as UserModel;
+                if (snapshot.exists())
+                {
+                    const user = snapshot.data() as UserModel;
 
-            return user.username;
-        }
+                    return user.username;
+                }
 
-        return null;
+                return null;
+            })
+        );
     }
 }
