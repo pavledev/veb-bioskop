3b5cfb7d4f6facb3d0d1b508b6f68e9deca7c7aa
Pavle <pavle_nis@yahoo.com>
2025-03-18 11:39:49 +0100

    Create cart.item.model.ts

---
+++ src/app/models/cart.item.model.ts
@@ -0,0 +1,11 @@
+export interface CartItemModel
+{
+    posterPath: string;
+    title: string;
+    cinemaLocation: string;
+    projectionDate: string;
+    projectionTime: string;
+    ticketCount: number;
+    technology: string;
+    price: number;
+}
