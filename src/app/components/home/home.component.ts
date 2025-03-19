fc1b6556dc2378534acbd09f78cd57a2b015a643
Pavle <pavle_nis@yahoo.com>
2025-03-19 07:48:25 +0100

    Add tabs to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MoviesComponent } from '../movies/movies.component';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [MatTabsModule, MatIconModule, MoviesComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
     providers: []
