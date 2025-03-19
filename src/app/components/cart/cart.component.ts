346d8be5de6282e66e88d990594c970c2c15dda9
Pavle <pavle_nis@yahoo.com>
2025-03-19 08:00:49 +0100

    Update cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,28 +6,66 @@ import { CartItemModel } from '../../models/cart.item.model';
 import { CartService } from '../../services/cart.service';
 import { UtilityService } from '../../services/utility.service';
 import { RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatError, MatFormField, MatLabel, MatPrefix } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import { MatOption } from '@angular/material/core';
+import { MatSelect } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'app-cart',
-    imports: [MatCardModule, MatIconModule, MatButtonModule, RouterLink, FormsModule, MatError, MatFormField, MatInput, MatLabel, MatPrefix, ReactiveFormsModule],
+    imports: [MatCardModule,
+        MatIconModule,
+        MatButtonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        MatInputModule,
+        RouterLink,
+        MatOption,
+        MatSelect],
     templateUrl: './cart.component.html',
     styleUrl: './cart.component.css',
-    providers: [CartService]
+    providers: [CartService, MovieService]
 })
-export class CartComponent
+export class CartComponent implements OnInit
 {
     private readonly formBuilder: FormBuilder = inject(FormBuilder);
     private readonly cartService: CartService = inject(CartService);
+    private readonly movieService: MovieService = inject(MovieService);
     public readonly utilityService: UtilityService = inject(UtilityService);
     public readonly cartForm: FormGroup;
 
-    public cartItems: CartItemModel[];
-
+    public readonly cartItems: CartItemModel[];
+    public cinemaLocations: string[] | null = null;
+    public error: string | null = null;
     constructor()
     {
+        this.cartForm = this.formBuilder.group({
+            cinemaLocation: ['', [Validators.required, Validators.minLength(2)]],
+            projectionDate: ['', [Validators.required, Validators.minLength(2)]],
+            projectionTime: ['', [Validators.required, Validators.email]],
+            ticketCount: ['', [Validators.required, Validators.minLength(6)]],
+            technology: ['', [Validators.required]]
+        });
+
         this.cartItems = this.cartService.getCartItems();
+    }
+
+    async ngOnInit()
+    {
+        const [error, response] = await this.movieService.getLocations();
+
+        if (error)
+        {
+            this.error = error.message;
+        }
+        else
+        {
+            this.cinemaLocations = response.data.map((location: any) => location.name);
+        }
     }
 }
