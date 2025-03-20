5a9429b942b6ce56ce7355cae84b91ca0947000e
Pavle <pavle_nis@yahoo.com>
2025-03-20 16:57:54 +0100

    Remove movieId, posterPath and dates properties from ProjectionModel and add halls property

--- src/app/models/projection.model.ts
+++ src/app/models/projection.model.ts
@@ -1,10 +1,8 @@
 export interface ProjectionModel
 {
-    movieId: string;
     title: string;
-    posterPath: string;
     price: number;
     maxTicketCount: number;
-    dates: string[];
     times: string[];
+    halls: string[];
 }
