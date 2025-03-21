717a3881c8c530f52c9d1586432046153f8dd353
Pavle <pavle_nis@yahoo.com>
2025-03-21 09:47:57 +0100

    Update cart.item.model.ts

--- src/app/models/cart.item.model.ts
+++ src/app/models/cart.item.model.ts
@@ -2,11 +2,4 @@ export interface CartItemModel
 {
     title: string;
     posterPath: string;
-    cinemaLocation: string;
-    projectionDate: string;
-    projectionTime: string;
-    hall: string;
-    ticketCount: number;
-    technology: string;
-    price: number;
 }
