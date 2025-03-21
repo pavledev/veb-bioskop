3477c006c810e2a7d0a725131e85edf55d3883c9
Pavle <pavle_nis@yahoo.com>
2025-03-21 09:49:23 +0100

    Add removeItem method to CartService

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -21,6 +21,18 @@ export class CartService
         return storedItems ? JSON.parse(storedItems) as CartItemModel[] : [];
     }
 
+    removeItem(index: number): void
+    {
+        const cartItems: CartItemModel[] = this.getCartItems();
+
+        if (index >= 0 && index < cartItems.length)
+        {
+            cartItems.splice(index, 1);
+
+            localStorage.setItem(this.CART_STORAGE_KEY, JSON.stringify(cartItems));
+        }
+    }
+
     clearCart(): void
     {
         localStorage.removeItem(this.CART_STORAGE_KEY);
