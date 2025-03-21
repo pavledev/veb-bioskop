bfe6e4db6cee656d63078a9d5799b6524bc8afd6
Pavle <pavle_nis@yahoo.com>
2025-03-21 09:47:40 +0100

    Update ReservationsComponent

--- src/app/components/user/reservations/reservations.component.ts
+++ src/app/components/user/reservations/reservations.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { ReservationModel } from '../../../models/reservation.model';
+import { ReservationService } from '../../../services/reservation.service';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-reservations',
-  imports: [],
-  templateUrl: './reservations.component.html',
-  styleUrl: './reservations.component.css'
+    selector: 'app-reservations',
+    templateUrl: './reservations.component.html',
+    imports: [
+        MatFormField,
+        MatInputModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule
+    ],
+    styleUrls: ['./reservations.component.scss'],
+    providers: [ReservationService]
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit
+{
+    displayedColumns: string[] = ['id', 'title', 'price', 'totalPrice', 'ticketCount', 'date', 'time', 'hall', 'technology', 'status'];
+    reservations = new MatTableDataSource<ReservationModel>([]);
 
+    @ViewChild(MatPaginator) paginator!: MatPaginator;
+    @ViewChild(MatSort) sort!: MatSort;
+
+    constructor(private reservationService: ReservationService)
+    {
+    }
+
+    async ngOnInit(): Promise<void>
+    {
+        this.reservationService.getAllReservations().subscribe(
+            (data) =>
+            {
+                console.log('Učitan broj rezervacija:', data.length);
+                console.log('Rezervacije:', data);
+                this.reservations.data = data;
+                this.reservations.paginator = this.paginator;
+                this.reservations.sort = this.sort;
+            },
+            (error) =>
+            {
+                console.error('Greška prilikom učitavanja rezervacija:', error);
+            }
+        );
+    }
+
+    applyFilter(event: Event)
+    {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.reservations.filter = filterValue.trim().toLowerCase();
+    }
 }
